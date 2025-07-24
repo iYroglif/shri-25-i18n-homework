681ba6f33ffd4d3e62185a59d629bcddbbef0567
iYroglif <webman.com@yandex.ru>
2025-07-24 18:46:57 +0300

    compile messages

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,13 @@ import svgr from "vite-plugin-svgr";
 // https://vite.dev/config/
 export default defineConfig({
     resolve: {
-        alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+        alias: [
+            { find: "@", replacement: path.resolve(__dirname, "src") },
+            {
+                find: "@formatjs/icu-messageformat-parser",
+                replacement: "@formatjs/icu-messageformat-parser/no-parser",
+            },
+        ],
     },
     plugins: [
         react({
