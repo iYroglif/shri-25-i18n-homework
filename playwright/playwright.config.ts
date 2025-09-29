f3e01ef9f00f3fb2905c8145b2df0350ab66e642
iYroglif <webman.com@yandex.ru>
2025-09-29 05:12:42 +0300

    fix

--- playwright/playwright.config.ts
+++ playwright/playwright.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     workers: process.env.CI ? 1 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: [["json", { outputFile: "test-results.json" }]],
-    maxFailures: 0,
+    maxFailures: 3,
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('/')`. */
